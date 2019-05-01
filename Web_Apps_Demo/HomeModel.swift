b6c83eb2c68f073ab0b8aab3303778e32115bf78
MLHLocalhost <localhost@mlh.io>
2019-05-01 18:32:27 -0400

    added files

---
+++ Web_Apps_Demo/HomeModel.swift
@@ -0,0 +1,89 @@
+//
+//  HomeModel.swift
+//  Web_Apps_Demo
+//
+//  Copyright © 2019 Mohammed Batarfi. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+protocol HomeModelProtocol: class {
+    func itemsDownloaded(items: NSArray)
+}
+
+
+class HomeModel: NSObject, URLSessionDataDelegate {
+    
+    //properties
+    
+    weak var delegate: HomeModelProtocol!
+    
+    let urlPath = "http://mjb.xkk.mybluehost.me/MySQL.php"
+    
+    func downloadItems() {
+        
+        let url: URL = URL(string: urlPath)!
+        let defaultSession = Foundation.URLSession(configuration: URLSessionConfiguration.default)
+        
+        let task = defaultSession.dataTask(with: url) { (data, response, error) in
+            
+            if error != nil {
+                print("Failed to download data")
+            }else {
+                print("Data downloaded")
+                self.parseJSON(data!)
+            }
+            
+        }
+        
+        task.resume()
+    }
+    
+    func parseJSON(_ data:Data) {
+        
+        var jsonResult = NSArray()
+        
+        do{
+            jsonResult = try JSONSerialization.jsonObject(with: data, options:JSONSerialization.ReadingOptions.allowFragments) as! NSArray
+            
+        } catch let error as NSError {
+            print(error)
+            
+        }
+        
+        var jsonElement = NSDictionary()
+        let locations = NSMutableArray()
+        
+        for i in 0 ..< jsonResult.count
+        {
+            
+            jsonElement = jsonResult[i] as! NSDictionary
+            
+            let location = LocationModel()
+            
+            //the following insures none of the JsonElement values are nil through optional binding
+            if let name = jsonElement["name"] as? String,
+                let address = jsonElement["address"] as? String,
+                let latitude = jsonElement["latitude"] as? String,
+                let longitude = jsonElement["longitude"] as? String
+            {
+                
+                location.name = name
+                location.address = address
+                location.latitude = latitude
+                location.longitude = longitude
+                
+            }
+            
+            locations.add(location)
+            
+        }
+        
+        DispatchQueue.main.async(execute: { () -> Void in
+            
+            self.delegate.itemsDownloaded(items: locations)
+            
+        })
+    }
+}
---
+++ Web_Apps_Demo/LocationModel.swift
@@ -0,0 +1,49 @@
+//
+//  LocationModel.swift
+//  Web_Apps_Demo
+//
+//  Copyright © 2019 Mohammed Batarfi. All rights reserved.
+//
+
+import UIKit
+
+import Foundation
+
+class LocationModel: NSObject {
+    
+    //properties
+    
+    var name: String?
+    var address: String?
+    var latitude: String?
+    var longitude: String?
+    
+    
+    //empty constructor
+    
+    override init()
+    {
+        
+    }
+    
+    //construct with @name, @address, @latitude, and @longitude parameters
+    
+    init(name: String, address: String, latitude: String, longitude: String) {
+        
+        self.name = name
+        self.address = address
+        self.latitude = latitude
+        self.longitude = longitude
+        
+    }
+    
+    
+    //prints object's current state
+    
+    override var description: String {
+        return "Name: \(String(describing: name)), Address: \(String(describing: address)), Latitude: \(String(describing: latitude)), Longitude: \(String(describing: longitude))"
+        
+    }
+    
+    
+}
